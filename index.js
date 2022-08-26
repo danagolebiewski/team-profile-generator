3398f296a1a5c55c3c437d1e5135e335b39241b2
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-08-26 14:21:44 -0600

    imported files to index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import Engineer from"./lib/Engineer.js";
+import Engineer from "./lib/Engineer.js";
+import Intern from "./lib/Intern.js";
+import Manger from "./lib/Manager.js";
+
+import inquirer from "inquirer";
+console.log("Hello World");
+
+
