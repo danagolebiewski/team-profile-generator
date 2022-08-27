7434993c804065a13ce89f2aa62269ddb7ba8a7d
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-08-26 18:11:54 -0600

    added questions to index.js file

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
-import Engineer from "./lib/Engineer.js";
-import Intern from "./lib/Intern.js";
-import Manger from "./lib/Manager.js";
+// import Engineer from "./lib/Engineer.js";
+// import Intern from "./lib/Intern.js";
+// import Manger from "./lib/Manager.js";
 
-import inquirer from "inquirer";
-console.log("Hello World");
+// import inquirer from "inquirer";
+// console.log("Hello World");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const engineer = new Engineer();
+const intern = new Intern ();
+const manager = new Manager();
+
+var questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "role",
+    message: "What is your role?",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your ID number?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "office",
+    message: "What is your office number?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub URL?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What school are you attending?",
+  },  
+]
+
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+};
+function init() {
+  inquirer.prompt(questions)
+  .then(answers => {
+    console.log("success");
+    writeToFile("index.html", generateMHTML({ ...answers}));
+});
+}
+init();
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,4 +1,4 @@
-const Employee = require ("../lib/Employee.js");
+const Employee = require("../lib/Employee.js");
 
 describe("Employee", () => {
   it("Should create an object with the name, id, and email if provided with valid arguments", () => {
@@ -8,7 +8,7 @@ describe("Employee", () => {
     expect(employee.id).toEqual(3);
     expect(employee.email).toEqual("dana.golebiewski@gmail.com");
   });
-// check if arguemtns are provided 
+  // check if arguemtns are provided
   it("should throw an error if no arguments provided", () => {
     const cb = new Employee();
 
@@ -17,24 +17,72 @@ describe("Employee", () => {
 
   // should include a name
   it("should throw an error if not provided a name", () => {
-    const cb = () => new Employee(3, "dana.golebiewski@gmail.com"); //is this how it should be written?? I'm going off of activity 16 from class 
+    const cb = () => new Employee(3, "dana.golebiewski@gmail.com"); //is this how it should be written?? I'm going off of activity 16 from class
 
     const err = new Error("Expected 'name' to be a string");
 
     expect(cb).toThrow(err);
   });
 
-  // should include an ID in the form of a number 
-  it("should throw an error if 'id'  ")
+  // should include an ID
+  it("should throw an error if 'id' is not provided", () => {
+    const cb = () => new Employee("Dana", "dana.golebiewski@gmail.com");
 
+    const err = new Error("Expect 'ID' to be a non-negative number");
 
-  // should include an email address 
+    expect(cb).toThrow(err);
+  });
 
+  // should include an email address
+  it("Should throw an error if email address in not in the correct format", () => {
+    const cb = () => new Employee("Dana", 3);
+
+    const err = new Error("Expect 'email' to be in correct format");
+
+    expect(cb).toThrow(err);
+  });
   // name should be a string
 
-// Id should be a number 
+  it("Should throw an errror if 'name' is not a string", () => {
+    const cb = () => new Employee(Dana, 3, "dana.golebiewski@gmail.com");
 
-// email should be an email address? Or string?
+    const err = new Error("Expect 'name' to be a string");
 
-// Should throw an error if ID is less than 0 
-})
+    expect(cb).toThrow(err);
+  });
+  // Id should be a number
+  it("Should throw an error if 'id' is not a number", () => {
+    const cb = () => new Employee("Dana", "3", "dana.golebiewski@gmail.com");
+
+    const err = new Error("Expect 'ID' to be a number");
+
+    expect(cb).toThrow(err);
+  });
+
+  // email should be an email address? Or string?
+  it("Should throw and error if 'email' is not in correct format", () => {
+    const cb = () => new Employee("Dana", 3, "dana.golebiewski@gmail");
+
+    const err = new Error("Expect 'email' to be a in the correct format");
+
+    expect(cb).toThrow(err);
+  });
+
+  // Should throw an error if ID is less than 0
+  it("Should throw and error if 'id' is less than 0", () => {
+    const cb = () => new Employee("Dana", -1, "dana.golebiewski@gmail");
+
+    const err = new Error("Expect 'id' to be a a non-negative number");
+
+    expect(cb).toThrow(err);
+  });
+  // should return Employee for role
+  it("Should throw an error ")
+});
+
+
+// How do I do the email check?, Do I need a test for returning employee? 
+// When I run npm test all of the test docs come up 
+// SOmeone had their test files as Employee.test.cjs
+// Why don't the links in index.js work?
+// index.js importing files problem - multiple ways - neither work 
