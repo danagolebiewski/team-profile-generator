3fbe3295159beb7438e60ab2b51bca693c600ef2
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-08-27 14:48:23 -0600

    moved all quetions into functions?

--- index.js
+++ index.js
@@ -56,13 +56,80 @@ function menu() {
 
   }
   function createEngineer(){
-
+    inquirer.prompt(
+      [
+        {
+          type: "input",
+          name: "name",
+          message: "What is your name?",
+        },
+        {
+          type: "input",
+          name: "role",
+          message: "What is your role?",
+          choices: ["Manager", "Engineer", "Intern"],
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is your ID number?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is your email address?",
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "What is your GitHub URL?",
+        },
+      ]
+    )
+    .then(answers => {
+      const engineer = new Engineer(answers.engName);
+      profile.push(engineer);
+    });
     teamOptions();
   }
   function createIntern(){
-
+    inquirer.prompt(
+      [
+        {
+          type: "input",
+          name: "name",
+          message: "What is your name?",
+        },
+        {
+          type: "input",
+          name: "role",
+          message: "What is your role?",
+          choices: ["Manager", "Engineer", "Intern"],
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is your ID number?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is your email address?",
+        },
+        {
+          type: "input",
+          name: "intern",
+          message: "What school are you attending?",
+        },
+      ]
+    )
+    .then(answers => {
+      const intern = new Intern(answers.intName);
+      profile.push(intern);
+    });
     teamOptions();
   }
+  
   function buildTeam(){
     if (!fs.existSync(outputDir)) {
       fs.mkdirSync(outputDir);
@@ -75,72 +142,3 @@ createManager();
 menu();
 
 
-
-inquirer.prompt(
-  [
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-    },
-    {
-      type: "input",
-      name: "role",
-      message: "What is your role?",
-      choices: ["Manager", "Engineer", "Intern"],
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is your ID number?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is your email address?",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "What is your GitHub URL?",
-    },
-  ]
-)
-.then(answers => {
-  const engineer = new Engineer(answers.engName);
-  profile.push(engineer);
-});
-inquirer.prompt(
-  [
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-    },
-    {
-      type: "input",
-      name: "role",
-      message: "What is your role?",
-      choices: ["Manager", "Engineer", "Intern"],
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is your ID number?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is your email address?",
-    },
-    {
-      type: "input",
-      name: "intern",
-      message: "What school are you attending?",
-    },
-  ]
-)
-.then(answers => {
-  const intern = new Intern(answers.intName);
-  profile.push(intern);
-});
